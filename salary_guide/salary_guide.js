ffb46f6202106293c8deaab09e372645b639dc48
Amir <amir.mohtasebi@gmail.com>
2018-03-26 23:43:14 +1300

    Some refactoring

--- salary_guide/salary_guide.js
+++ salary_guide/salary_guide.js
@@ -1,161 +1,163 @@
-function goBack() {
-      var parent = document.getElementById("parent").value;
-      draw(+parent);
-}
+var SalaryGuide = (function() {
+  function SalaryGuide(fileName, title) {
+    this.fileName = fileName;
+    this.title = title;
+    this.draw(5000);
+  };
 
-var getPreviousParent = function(data, parent) {
-  var previousParent = data.filter(function(d) { return d.CategoryId === parent; });
-  if (previousParent[0]) {
-    return previousParent[0];
-  } else {
-    return Object.create({}, { Parent: { value: 5000 }, Category: { value: 'All Categories' }  });
+  SalaryGuide.prototype.parent = function() {
+    return document.getElementById("parent").value;
   }
-}
 
-function draw(parent) {
-  d3.tsv('salary_guide_data.tsv', type, function(error, sourceData) {
-    var data = sourceData.filter(function(d) { return d.Parent === parent; });
-    var previousParent = getPreviousParent(sourceData, parent);
-    document.getElementById("parent").value = previousParent.Parent;
-    var margin = {top: 60, right: 50, bottom: 125, left: 50},
-    width = 936 - margin.left - margin.right,
-    height = 550 - margin.top - margin.bottom;
+  SalaryGuide.prototype.goBack = function(target) {
+    var parent = document.getElementById("parent").value;
+    this.draw(+parent);
+  };
 
-    var x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
-    var y = d3.scaleLinear().range([height, 0]);
+  SalaryGuide.prototype.getPreviousParent = function(data, parent) {
+    var previousParent = data.filter(function(d) { return d.CategoryId === parent; });
+    if (previousParent[0]) {
+      return previousParent[0];
+    } else {
+      return Object.create({}, { Parent: { value: 5000 }, Category: { value: 'All Categories' }  });
+    }
+  };
 
-    var xAxis = d3.axisBottom(x);
-    var yAxis = d3.axisLeft(y);
+  SalaryGuide.prototype.draw = function(parent) {
+    var that = this;
+    d3.tsv(this.fileName, this.type, function(error, sourceData) {
+      var data = sourceData.filter(function(d) { return d.Parent === parent; });
+      var previousParent = that.getPreviousParent(sourceData, parent);
+      document.getElementById("parent").value = previousParent.Parent;
+      var margin = {top: 60, right: 50, bottom: 125, left: 50},
+      width = 936 - margin.left - margin.right,
+      height = 550 - margin.top - margin.bottom;
 
-    var tip = d3.tip()
-      .attr('class', 'd3-tip')
-      .offset([-10, 0])
-      .html(function(d) {
-        return "Max: " + d.Max + "<br/>" + "Median: " + d.Median + "<br/>" + "Min: " + d.Min + "<br/>";
-      });
+      var x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
+      var y = d3.scaleLinear().range([height, 0]);
 
-    var xDomain = function(data) { return data.map(function(d) { return d.Category; }); },
-        yDomain = function(data) { return [0, d3.max(data, function(d) { return d.Max; })]; },
-        categryMap = function(data) { return x(data.Category) + (x.bandwidth() - 26)/2; },
-        maxMap = function(data) { return y(data.Max); },
-        medianMap = function(data) { return y(data.Median); },
-        barHeight = function(data) { return (height - y(data.Max)) - (height - y(data.Min)); },
-        medianBarHeight = function(data) { return (5); },
-        drillDown = function(d) {
-            if (!d.Leaf) {
-              tip.hide();
-              draw(d.CategoryId); 
-            } 
-          };
+      var xAxis = d3.axisBottom(x);
+      var yAxis = d3.axisLeft(y);
 
-    x.domain(xDomain(data));
-    y.domain(yDomain(data));
+      var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+          return "Max: " + d.Max + "<br/>" + "Median: " + d.Median + "<br/>" + "Min: " + d.Min + "<br/>";
+        });
 
-    d3.select('.chart').select('g').remove();
+      var xDomain = function(data) { return data.map(function(d) { return d.Category; }); },
+          yDomain = function(data) { return [0, d3.max(data, function(d) { return d.Max; })]; },
+          categryMap = function(data) { return x(data.Category) + (x.bandwidth() - 26)/2; },
+          maxMap = function(data) { return y(data.Max); },
+          medianMap = function(data) { return y(data.Median); },
+          barHeight = function(data) { return (height - y(data.Max)) - (height - y(data.Min)); },
+          medianBarHeight = function(data) { return (5); },
+          drillDown = function(d) {
+              if (!d.Leaf) {
+                tip.hide();
+                that.draw(d.CategoryId); 
+              } 
+            };
 
-    var chartWidth = width + margin.left + margin.right;
-    var chartHeight = height + margin.top + margin.bottom;
+      x.domain(xDomain(data));
+      y.domain(yDomain(data));
 
-      var chart = d3.select('.chart')
-          .attr('viewBox', '0 0 ' + chartWidth + ' ' + chartHeight)
-          .attr('preserveAspectRatio', 'xMidYMid meet')
-          .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        
-      chart.call(tip);
+      d3.select('.chart').select('g').remove();
 
-      chart.append("text")
-          .attr("x", (width / 2))
-          .attr("y", 0 - (margin.top / 2))
-          .attr("text-anchor", "middle")
-          .style("fill", "black")
-          .style("font-size", "16px")
-          .style("text-decoration", "none")
-          .text("NZ Salary Guide (Oct 2016 - Mar 2017) - " + previousParent.Category);
+      var chartWidth = width + margin.left + margin.right;
+      var chartHeight = height + margin.top + margin.bottom;
 
-      chart.append("text")
-          .attr("x", (width / 2))
-          .attr("y", 0 - (margin.top / 2) + 20)
-          .attr("text-anchor", "middle")
-          .attr("onclick", "goBack()")
-          .attr('class', 'hand-cursor back-button')
-          .style("font-size", "16px")
-          .style("text-decoration", "none")
-          .style("display", parent === 5000 ? 'none' : 'block')
-          .text("(Go Up)");
+        var chart = d3.select('.chart')
+            .attr('viewBox', '0 0 ' + chartWidth + ' ' + chartHeight)
+            .attr('preserveAspectRatio', 'xMidYMid meet')
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+          
+        chart.call(tip);
 
-      chart.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis)
-          .selectAll("text")
-          .style("text-anchor", "start")    
-          .attr("y", 0)
-          .attr("x", 7)
-          .attr("transform" , "rotate(45)");
+        chart.append("text")
+            .attr("x", (width / 2))
+            .attr("y", 0 - (margin.top / 2))
+            .attr("text-anchor", "middle")
+            .style("fill", "black")
+            .style("font-size", "16px")
+            .style("text-decoration", "none")
+            .text(that.title + ' - ' + previousParent.Category);
 
-      var yAxisG =chart.append("g")
-              .attr("class", "y axis")
-              .call(yAxis);
-          yAxisG.selectAll("text")
-            .attr("dx", "-1.71em");
-          yAxisG.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", ".71em")
-            .style("text-anchor", "end")
-            .text("Salary ($NZD)");
+        chart.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .style("text-anchor", "start")    
+            .attr("y", 0)
+            .attr("x", 7)
+            .attr("transform" , "rotate(45)");
 
-      var g = chart.selectAll(".bar")
-          .data(data)
-          .enter()
-          .append("g");
+        var yAxisG = chart.append("g")
+                .attr("class", "y axis")
+                .call(yAxis);
+            yAxisG.selectAll("text")
+              .attr("dx", "-1.71em");
+            yAxisG.append("text")
+              .attr("transform", "rotate(-90)")
+              .attr("y", 6)
+              .attr("dy", ".71em")
+              .style("text-anchor", "end")
+              .text("Salary ($NZD)");
 
-      g.append("rect")
-          .attr("class", function(d) { return d.Leaf ? 'bar' : 'bar hand-cursor'; })
-          .attr("x", categryMap)
-          .attr("y", maxMap)
-          .attr("height", 0)
-          .attr("height", barHeight)
-          .on('click', function(d) { return drillDown(d); })
-          .attr("width", 0)
-          .transition()
-          .delay(function (d, i) {
-            return i * 25;
-          })
+        var g = chart.selectAll(".bar")
+            .data(data)
+            .enter()
+            .append("g");
+
+        g.append("rect")
+            .attr("class", function(d) { return d.Leaf ? 'bar' : 'bar hand-cursor'; })
+            .attr("x", categryMap)
+            .attr("y", maxMap)
+            .attr("height", 0)
+            .attr("height", barHeight)
+            .on('click', function(d) { return drillDown(d); })
+            .attr("width", 0)
+            .transition()
+            .delay(function (d, i) {
+              return i * 25;
+            })
+              .attr('width', 26);
+
+        g.append("rect")
+            .attr("class", function(d) { return d.Leaf ? 'medianBar' : 'medianBar hand-cursor'; })
+            .attr("x", categryMap)
+            .attr("y", medianMap)
+            .attr("height", medianBarHeight)
+            .on('click', function(d) { return drillDown(d); })
+            .transition()
+            .duration(200)
+            .delay(function (d, i) {
+              return i * 25;
+            })
+            .attr("width", 0)
+            .transition()
+            .delay(function (d, i) {
+              return i * 25;
+            })
             .attr('width', 26);
 
-      g.append("rect")
-          .attr("class", function(d) { return d.Leaf ? 'medianBar' : 'medianBar hand-cursor'; })
-          .attr("x", categryMap)
-          .attr("y", medianMap)
-          .attr("height", medianBarHeight)
-          .on('click', function(d) { return drillDown(d); })
-          .transition()
-          .duration(200)
-          .delay(function (d, i) {
-            return i * 25;
-          })
-          .attr("width", 0)
-          .transition()
-          .delay(function (d, i) {
-            return i * 25;
-          })
-          .attr('width', 26);
+        g.on('mouseover', tip.show);
+        g.on('mouseout', tip.hide);
+    });
+  }
 
-      g.on('mouseover', tip.show);
-      g.on('mouseout', tip.hide);
-});
-}
+  SalaryGuide.prototype.type = function(d) {
+    d.Median = +d.Median;
+    d.Min = +d.Min;
+    d.Max = +d.Max;
+    d.Parent = +d.Parent
+    d.CategoryId = +d.CategoryId
+    d.Leaf = d.Leaf === 'true'
+    return d;
+  }
 
-function type(d) {
-  d.Median = +d.Median;
-  d.Min = +d.Min;
-  d.Max = +d.Max;
-  d.Parent = +d.Parent
-  d.CategoryId = +d.CategoryId
-  d.Leaf = d.Leaf === 'true'
-  return d;
-}
-
-draw(5000);
+  return SalaryGuide;
+}());
